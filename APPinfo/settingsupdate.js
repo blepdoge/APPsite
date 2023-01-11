431aca6d188651a47ed218fe5e84a74e627f2f8a
Louis-Marie <blep.doge@gmail.com>
2023-01-11 09:23:43 +0100

    revamp de la generation des listes user

--- APPinfo/settingsupdate.js
+++ APPinfo/settingsupdate.js
@@ -24,7 +24,7 @@ form.addEventListener("submit", (event) => {
     // Check the status of the request
     if (xhr.status === 200) {
       // Display the result of the PHP script
-      document.getElementById("resultat").innerHTML = xhr.responseText + "Pour afficher les valeurs mises à jour, veuillez vous reconnecter.";
+      document.getElementById("resultat").innerHTML = xhr.responseText + "Les valeurs ont bien été mises à jour.";
     } else {
       // Display an error message
       document.getElementById("resultat").innerHTML = "Une erreur est survenue.";
