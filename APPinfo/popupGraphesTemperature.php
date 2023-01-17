faae897297c1fafb36588616a5890b424ad937bd
Louis-Marie <blep.doge@gmail.com>
2023-01-17 18:52:33 +0100

    les grapheeees

--- APPinfo/popupGraphesTemperature.php
+++ APPinfo/popupGraphesTemperature.php
@@ -1,9 +1,24 @@
+<?php
+$currentBoxID = urldecode($_GET["currentBox"]);
+
+require_once "config.php";
+$query = "SELECT sensorvalues.* FROM sensorvalues INNER JOIN labboxtable ON sensorvalues.LabBoxTable_idLabBox = labboxtable.idLabBox WHERE labboxtable.nomBox='$currentBoxID'";
+$data = mysqli_query($link, $query);
+
+foreach ($data as $row) {
+  $temp[] = $row['Tempvalue'];
+
+  $timestamp[] = $row['timestamp'];
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Température</title>
-    <link rel="stylesheet" href="assets/css/popupGraphesStyle.css" />
     <meta charset="utf-8" />
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <link rel="stylesheet" href="assets/css/popupGraphesStyle.css" />
   </head>
 
 <body style="margin: 0">
@@ -16,10 +31,35 @@
         <div class = "GraphetValeurText">
           <p class = "valeurGraphe">20°C<br><br>Valeur min :<br>19°C<br><br>Valeur max :<br>21°C</p>
           <img class = "ligne" src="assets/images/Line.png" alt="Graph" />
-          <img class = "GraphTemplate" src="assets/images/GraphTemplate.png" alt="Graph" />  
+          <div class="canvasContainer">
+          <canvas id="myChart"></canvas>
+        </div>  
         </div> 
       </div> 
   </div>
+
+  <script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: <?php echo json_encode($timestamp); ?>,
+      datasets: [{
+        label: 'Température',
+        data: <?php echo json_encode($temp); ?>,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
 
 </body>
 </html>
--- APPinfo/settingsupdate.php
+++ APPinfo/settingsupdate.php
@@ -2,6 +2,15 @@
 
 require_once "config.php";
 
+$oldNomLabo = $_SESSION['nomLabo'];
+$oldAdresseLabo = $_SESSION['adresseLabo'];
+$oldEmailLabo = $_SESSION['emailLabo'];
+$oldNom = $_SESSION['nom'];
+$oldPrenom = $_SESSION['prenom'];
+$oldEmail = $_SESSION['email'];
+$oldAdresse = $_SESSION['adresse'];
+
+
 // Get the submitted data
 $nomLabo = mysqli_real_escape_string($link, $_POST['nomLabo']);
 $adresseLabo = mysqli_real_escape_string($link, $_POST['adresseLabo']);
@@ -12,32 +21,32 @@ $email = mysqli_real_escape_string($link, $_POST['email']);
 $adresse = mysqli_real_escape_string($link, $_POST['adresse']);
 $motdepasse = mysqli_real_escape_string($link, $_POST['motdepasse']);
 
-$_SESSION['nom'] = $nom;
-$_SESSION['prenom'] = $prenom;
-$_SESSION['email'] = $email;
-$_SESSION['adresse'] = $adresse;
-
 
 // Check if the data from the laboratoires table needs to be updated
-$query = "SELECT nomLabo, adresseLabo, emailLabo FROM laboratoires WHERE emailLabo='$emailLabo' AND nomLabo='$nomLabo' AND adresseLabo='$adresseLabo'";
+$query = "SELECT nomLabo, adresseLabo, emailLabo FROM laboratoires WHERE emailLabo='$oldEmailLabo' AND nomLabo='$oldNomLabo' AND adresseLabo='$oldAdresseLabo'";
 $result = mysqli_query($link, $query);
 
 if ($result->num_rows == 0) {
   // Update the laboratoires table
-  $query = "UPDATE laboratoires SET nomLabo='$nomLabo', adresseLabo=' $adresseLabo', emailLabo='$emailLabo' WHERE emailLabo='$emailLabo'";
+  $query = "UPDATE laboratoires SET nomLabo='$nomLabo', adresseLabo=' $adresseLabo', emailLabo='$emailLabo' WHERE emailLabo='$oldEmailLabo'";
   $result = mysqli_query($link, $query);
   echo "Données du laboratoire changées. ";
 }
 
 // Check if the data from the users table needs to be updated
-$query = "SELECT nom, prenom, email, adresse FROM users WHERE email='$email' AND nom='$nom' AND prenom='$prenom' AND adresse='$adresse'";
+$query = "SELECT nom, prenom, email, adresse FROM users WHERE email='$oldEmail' AND nom='$oldNom' AND prenom='$oldPrenom' AND adresse='$oldAdresse'";
 $result = mysqli_query($link, $query);
 
 if ($result->num_rows == 0) {
   // Update the users table
-  $query = "UPDATE users SET nom='$nom', prenom='$prenom', email='$email', adresse='$adresse' WHERE email='$email'";
+  $query = "UPDATE users SET nom='$nom', prenom='$prenom', email='$email', adresse='$adresse' WHERE email='$oldEmail'";
   $result = mysqli_query($link, $query);
+  $_SESSION['nom'] = $nom;
+  $_SESSION['prenom'] = $prenom;
+  $_SESSION['email'] = $email;
+  $_SESSION['adresse'] = $adresse;
   echo "Données utilisateur changées. ";
+  
 }
 
 //changement mdp
