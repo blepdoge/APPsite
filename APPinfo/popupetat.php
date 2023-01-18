af6e37a43ac4e36e65acbbde879d461f815d212c
Mathilde-lelephant <116710739+Mathilde-lelephant@users.noreply.github.com>
2023-01-18 10:58:35 +0100

    modifier le nom des box

--- APPinfo/popupetat.php
+++ APPinfo/popupetat.php
@@ -8,35 +8,68 @@
 </head>
 
 <body>
+
+
+<?php
+session_start();
+// Connexion a notre bdd
+require_once "config.php";
+//form pour changer le nom
+
+
+
+
+$nomBox_err = "";
+
+//traitement du form
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        
+  if (empty(trim($_POST["boxNameChanged"]))) {
+    $nomBox_err = "Vous n'avez pas entré de nom";
+  } else {
+    $newnomBox = htmlspecialchars(trim($_POST["boxNameChanged"]));
+  }
+
+    $oldnomBox = $_SESSION["old"];
+
+  if (empty($nomBox_err)) {
+    // Ajouter le nom de la box dans la base de données
+    $query1 = "UPDATE labboxtable SET nombox = '$newnomBox' WHERE nombox = '$oldnomBox' AND laboratoires_idlaboratoires = ".$_SESSION["idLabo"];
+    mysqli_query($link, $query1);
+    $_SESSION["old"] = $newnomBox;
+  }
+
+}
+ 
+
+?>
     <div class="titre">
     <h1 id="titre">
     </h1>
     <?php
 
-    // Connexion a notre bdd
-    require_once "config.php";
-
-    $newnomBox = "";
-    $nomBox_err = "";
-
-    //form pour changer le nom
-    $nomBox = urldecode($_GET["currentBoxID"]);
+    if(!isset($oldnomBox)){
+        $oldnomBox = urldecode($_GET["currentBoxID"]);
+        $_SESSION["old"] = $oldnomBox;
+    }
     
-    echo '<form method="POST" action="Modifnombox.php?nombox="'.$nomBox.'">
-    <input type="text" value="'.$nomBox.'" id="nomLabBox" name="boxNameChanged"></input>
+    
+    echo '<form method="POST" action='.htmlspecialchars($_SERVER["PHP_SELF"]).'>
+    <input type="text" value="'.$_SESSION["old"] .'" id="nomLabBox" name="boxNameChanged"></input>
     <input type="submit" value="Modifier le nom de la box"></input> 
     </form>';
-    echo $nomBox_err;
 
      
 
     // faire la requete sql en fonction du labo de la session actuelle
-    $query = "SELECT LocalIP FROM labboxtable WHERE nomBox ='" . $nomBox . "'";
+    $query = "SELECT LocalIP FROM labboxtable WHERE nomBox ='" .$_SESSION["old"]  . "'";
     $resultatquery = mysqli_query($link, $query);
     $adresseIP = mysqli_fetch_array($resultatquery)[0];
 
-    // fermer bdd
-    mysqli_close($link);
+    
+    // Close connection
+  mysqli_close($link);
 
     ?>
         
