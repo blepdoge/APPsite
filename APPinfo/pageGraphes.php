80c9e9199b28f80e8604bfcb9e7d4bdd6eeba170
Tiffany <tiffanyreypro@gmail.com>
2023-01-20 00:21:23 +0100

    header connecté?

--- APPinfo/pageGraphes.php
+++ APPinfo/pageGraphes.php
@@ -20,62 +20,7 @@ $currentBox = urldecode($_GET["currentBoxID"]);
   <meta charset="utf-8" />
 </head>
 
-<header>
-  <div class="sideheader">
-    <a href="pageAccueil.html"><img class="logotop" src="assets/images/logosorsen_crop.png" width="60"
-        height="60" /></a>
-
-    <div class="textelogo">
-      <p style="margin: 18px">SORSEN</p>
-    </div>
-  </div>
-
-
-  <div class="userSearchBar">
-    <form method="GET" action="userList.php" id="searchForm"> <!--rediriger vers userSearch avec les params en head-->
-
-      <input type="text" id="userSearchBar" name="userSearchBar" placeholder="Chercher un utilisateur">
-      <button type="submit" id="searchUser" name="searchUser">Chercher</button>
-
-    </form>
-  </div>
-
-  <script>
-    function showOptions() {
-
-      const element = document.querySelector(".options");
-      if (element.classList.contains('show') == false) {
-        element.classList.add("show");// affiche le menu
-      } else {
-        element.classList.remove("show");
-      }
-      // cache le menu
-      //ce menu vous est apporté par louis-marie
-    }
-  </script>
-
-  <nav>
-    <ul class="navlien">
-      <li><a href="FAQ.php">FAQ</a></li>
-      <!--mettre le lien vers la FAQ-->
-      <li style="color: gray">|</li>
-      <li id="monCompte" onclick="showOptions()">
-        <a>Mon compte</a>
-      </li>
-      <!--mettre le lien vers la page de co-->
-    </ul>
-  </nav>
-
-  <div class="options">
-    <ul>
-      <li><a href="pageSettings.php">Paramètres</a></li>
-      <li><a href="ContrôleBox.php">Accueil</a></li>
-      <li><a href="logout.php">Déconnexion</a>
-        <!--ici faudra link le logout.php-->
-      </li>
-    </ul>
-  </div>
-</header>
+<?php include_once "views/LoggedINHeader.php" ?>
 
 <script>
   function display_c() {
